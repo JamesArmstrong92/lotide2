789ddfd9cfc2db502ba0809b483a99ab1522f1ab
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-01 12:57:34 -0500

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+//assertEqual function
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+
+};
+
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('1, 2, 3', [1, 2, 3]);
+assertEqual('Hello', 'Hello');
