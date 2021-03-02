8ca88888edf139955143a302d4fcab0c4941b80a
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-02 16:43:45 -0500

    Refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,7 +19,7 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
 
-  if (arr1 === arr2) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`Assertion Passed✅✅✅: ${arr1} === ${arr2}`);
   } else
     console.log(`Assertion Failed🚨🚨🚨: ${arr1} === ${arr2}`);
