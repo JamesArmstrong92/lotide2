329aca9339318dc453115df398c5ead480f604c0
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-04 17:07:34 -0500

    Added asserEqual

--- findKey.js
+++ findKey.js
@@ -1,15 +1,15 @@
-// const assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
 
-//   if (actual === expected) {
-//     console.log(`Assertion Passed✅✅✅: ${actual} === ${expected}`);
-//   } else
-//     console.log(`Assertion Failed🚨🚨🚨: ${actual} !== ${expected}`);
+  if (actual === expected) {
+    console.log(`Assertion Passed✅✅✅: ${actual} === ${expected}`);
+  } else
+    console.log(`Assertion Failed🚨🚨🚨: ${actual} !== ${expected}`);
 
-// };
+};
 
 const findKey = function(object, callback) {
 
-  
+
   for (let key in object) {
     if (callback(object[key])) {
       console.log('key ---> ',key);
@@ -20,11 +20,11 @@ const findKey = function(object, callback) {
 
 
 
-findKey({
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2); // => "noma"
+}, x => x.stars === 1)); // => "noma"
