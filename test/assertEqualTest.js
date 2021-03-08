914fff4f0392ec3a3cd9b512a93b71140bb08bf4
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-08 08:56:59 -0500

    Test folder

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('1, 2, 3', [1, 2, 3]);
+assertEqual('Hello', 'Hello');
+assertEqual("Lighthouse Labs", "Bootcamp");
